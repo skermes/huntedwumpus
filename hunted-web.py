ea42316b8df5ebc9523309f94e98b535886a37a4
Sean Kermes <skermes@gmail.com>
2010-05-16 18:15:30 -0400

    Getting the web frontend to create a new cavern and report its initial state.

--- hunted-web.py
+++ hunted-web.py
@@ -1,4 +1,5 @@
 import web
+import hunted
 
 urls = (
     '/wumpus', 'Wumpus'
@@ -7,7 +8,21 @@ app = web.application(urls, globals())
 
 class Wumpus:
     def GET(self):
-        return "Hello, Wumpus." 
+        cavern = hunted.begin()
+        builder = StringBuilder()
+        cavern['output'] = builder
+        hunted.look(cavern)
+        return str(builder)
         
+class StringBuilder:
+    def __init__(self):
+        self.__value = ''
+       
+    def write(self, data):
+        self.__value += str(data)
+        
+    def __str__(self):
+        return self.__value
+       
 if __name__ == '__main__':
     app.run()
