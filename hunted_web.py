afcee4f7d74f5065a2bff4c9cd8fdf55e6f1baa0
Sean Kermes <skermes@gmail.com>
2010-05-16 21:24:11 -0400

    With the addition of a few hacks (including one whopping great security hole) I can now post moves back to the server and play a game.

--- hunted_web.py
+++ hunted_web.py
@@ -9,12 +9,33 @@ render = web.template.render('templates')
 
 class Wumpus:
     def GET(self):
-        cavern = hunted.begin(output=StringBuilder())        
+        cavern = hunted.begin(output=StringBuilder())
+        cavern['output'] = str(cavern['output'])
+        return render.hunted(cavern)
+        
+    def POST(self):
+        data = web.input()
+        # TODO: Find a way to keep cavern state in the page 
+        # that isn't vulnerable to hideous security holes.
+        cavern = eval(data.cavern)
+        cavern['output'] = StringBuilder(initial=cavern['output'])
+        action = data.action
+        if data.argument is not None and data.argument != '':
+            argument = int(data.argument)
+        else:
+            argument = data.argument
+        
+        hunted.tell(cavern, '')
+        cavern = hunted.do(action, argument, cavern)
+        hunted.look(cavern)
+        hunted.tell(cavern, '')
+        
+        cavern['output'] = str(cavern['output'])
         return render.hunted(cavern)
         
 class StringBuilder:
-    def __init__(self):
-        self.__value = ''
+    def __init__(self, initial=''):
+        self.__value = initial
        
     def write(self, data):
         self.__value += str(data)
