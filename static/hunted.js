beb7653cef35239d4d0b0160d64129d1c98b6961
Sean Kermes <skermes@gmail.com>
2010-05-17 20:56:49 -0400

    Adding some jquery magic to get the focus transitioning between inputs on enter.

--- static/hunted.js
+++ static/hunted.js
@@ -1,9 +1,21 @@
 
 function enterAction(e) {
-    e.preventDefault();
+    var action = $("#action-input").val()
+    
+    if (action == "s" || action == "m")
+    {
+        e.preventDefault();
+        
+        if (action == "s")
+            $("label[for='argument-input']").text("How long?");
+        else
+            $("label[for='argument-input']").text("Where to?");
+            
+        $("#argument-input").focus();
+    }    
 }
 
 $(document).ready(function() {
-    $("input[type='submit']").click(enterAction);
-    $("#action-input").focus();    
+    $("#enter").click(enterAction);
+    $("#action-input").focus();        
 });
